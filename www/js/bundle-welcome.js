28de66a16cee61d635c314c4343bc166579bece4
davemanuel <jdmanuel242@gmail.com>
2019-01-09 16:28:46 +0800

    fixes

--- www/js/bundle-welcome.js
+++ www/js/bundle-welcome.js
@@ -155,6 +155,7 @@
                 },
                 'onClosed': function() {
                     console.log("welcome screen closed");
+                     app.dialog.alert('You need a torrent client to download your favorite movies.');
                 }
             };
 
